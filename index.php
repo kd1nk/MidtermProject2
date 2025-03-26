2506382899e44af0d07bb5a92d9aec49b2821fbb
Kacy Dinkel <kdinkel03@gmail.com>
2025-03-26 14:40:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 include 'main.php'; // Replace with the actual name of your API file.
+
+header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    if ($method === 'OPTIONS') {
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+        exit();
+    }
 ?>
